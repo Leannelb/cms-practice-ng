5c79893afaf126135cb4dc78440fb54a0e49f25d
Leannelb <leannelaceybyrne@outlook.com>
2019-02-02 19:10:02 +0100

    added import script and ts code for googlemaps api

--- cms/src/app/components/screens/cleaners/cleaners-map/cleaners-map.component.ts
+++ cms/src/app/components/screens/cleaners/cleaners-map/cleaners-map.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+<reference types="@types/googlemaps"/>>
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cleaners-map',
-  templateUrl: './cleaners-map.component.html',
-  styleUrls: ['./cleaners-map.component.css']
+  templateUrl: './cleaners-map.component.html'
 })
 export class CleanersMapComponent implements OnInit {
+
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
 
   constructor() { }
 
   ngOnInit() {
-  }
+    var mapProp = {
+      center: new google.maps.LatLng(35.912224, 14.504167),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
 
+  }
+  setMapType(e:any,mapTypeId: string) {
+    e.preventDefault();
+    this.map.setMapTypeId(mapTypeId)    
+  }
 }
