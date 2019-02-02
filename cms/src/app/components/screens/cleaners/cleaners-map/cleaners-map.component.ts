4d884eed3b56386d78884423333b1902e2bf8e45
Leannelb <leannelaceybyrne@outlook.com>
2019-02-02 18:51:14 +0100

    adding cleaners map and routing to cleanersModule

---
+++ cms/src/app/components/screens/cleaners/cleaners-map/cleaners-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cleaners-map',
+  templateUrl: './cleaners-map.component.html',
+  styleUrls: ['./cleaners-map.component.css']
+})
+export class CleanersMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- cms/src/app/components/screens/cleaners/cleaners.module.ts
+++ cms/src/app/components/screens/cleaners/cleaners.module.ts
@@ -4,11 +4,13 @@ import { CleanersListingComponent } from './cleaners/crud/cleaners-listing/clean
 import { CleanersEditComponent } from './cleaners/crud/cleaners-edit/cleaners-edit.component';
 import { CleanersNewComponent } from './cleaners/crud/cleaners-new/cleaners-new.component';
 import { CommonModule } from '@angular/common';
+import { CleanersMapComponent } from './cleaners-map/cleaners-map.component';
 
 const routes: Routes = [
   { path: '', component: CleanersListingComponent },
   { path: 'details/:ref', component: CleanersEditComponent },
   { path: 'new', component: CleanersNewComponent },
+  { path: 'map', component: CleanersMapComponent },
 
 
 ]
@@ -24,7 +26,8 @@ const routes: Routes = [
   declarations: [ 
     CleanersListingComponent,
     CleanersEditComponent,
-    CleanersNewComponent
+    CleanersNewComponent,
+    CleanersMapComponent
   ]
 })
 export class CleanersModule { }
